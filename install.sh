341e6d42c030ba734439657342ccb288755d325a
m4ppi <kazuki.matsumoto@fox-hound.tech>
2018-04-20 22:39:09 +0900

    update

--- install.sh
+++ install.sh
@@ -4,6 +4,32 @@ set -eu
 
 # 実行場所のディレクトリを取得
 THIS_DIR=$(cd $(dirname $0); pwd)
+DOT_DIRECTORY="${HOME}/dotfiles"
+DOT_TARBALL="https://github.com/m4ppi/dotfiles.git"
+REMOTE_URL="git@github.com:m4ppi/dotfiles.git"
+# if directory does not exist
+if [ ! -d ${DOT_DIRECTORY} ]; then
+    echo "Downloading dotfiles..."
+    mkdir ${DTO_DIRECTORY}
+
+    if type "git" > /dev/null 2>&1; then
+        git clone --recursive "${REMOTE_URL}" "${DOT_DIRECTORY}"
+    else
+        curl -fsSlo ${HOME}/dotfiles.tar.gz ${DOT_TARBALL}
+        tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOT_DIRECTORY}
+        rm -f ${HOME}/dotfiles.tar.gz
+    fi
+
+    echo $(tput setaf 2)Download dotfiles complete!. ✔︎$(tput sgr0)
+fi
+
+# コマンドの存在確認
+# has() {
+  # type "$1" > /dev/null 2>&1
+# }
+# if has "brew"; then
+  # ...
+# fi
 
 cd $THIS_DIR
 git submodule init
